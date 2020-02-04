f6e6152c6334a2f741aabd6868e9a58c6d4f7d27
최예슬 <seulshine@gmail.com>
2020-02-04 16:33:24 +0900

    S02P13A303-13 S02P13A303-14 다시 커밋

--- Front_End/Provider.tsx
+++ Front_End/Provider.tsx
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import configureStore from "./stores/login_form/store/configure";
+import configureStore from "./stores/login/store/configure";
 import App from "./src/App";
-import Navbar from "./src/components/Navbar";
-import Footer from "./src/components/Footer";
-import Section from "./src/components/Section";
+import Navbar from "./src/components/App/Navbar";
+import Footer from "./src/components/App/Footer";
+import Section from "./src/components/App/Section";
 import { BrowserRouter, Route } from "react-router-dom";
 import MyReview from "./src/components/MyReview";
 import List from "./src/components/List_page/List";
-import WineDetail from "./src/components/WineDetail";
-import Entrance from "./src/components/Entrance";
-
+import WineDetail from "./src/components/WineDetail/WineDetail";
+import Entrance from "./src/components/App/Entrance";
+// import SignUp from "./stores/login/components/SignUp";
+import SignUp from "./src/components/App/SignUp";
 class AppForm extends Component {
   public store = (): object => {
     const store = Object.assign({}, this.props);
@@ -27,7 +28,7 @@ class AppForm extends Component {
           <Route path="/myreview" component={MyReview} />
           <Route path="/list" component={List} />
           <Route path="/detail" component={WineDetail} />
-         
+          <Route path="/signUp" component={SignUp} />
           <Footer />
         </BrowserRouter>
       </Provider>
