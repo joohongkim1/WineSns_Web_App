f0c8a89ff5b861bb410dcee30b415fea6f8b941c
김병인 <skydevilk@gmail.com>
2020-02-04 16:27:03 +0900

    finished email certify

--- Back_End/src/main/java/com/ssafy/wine/controller/SignController.java
+++ Back_End/src/main/java/com/ssafy/wine/controller/SignController.java
@@ -1,7 +1,12 @@
 package com.ssafy.wine.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.wine.advice.exception.CEmailSigninFailedException;
 import com.ssafy.wine.dto.User;
+import com.ssafy.wine.email.MailUtils;
+import com.ssafy.wine.email.TempKey;
 import com.ssafy.wine.handler.CommonResult;
 import com.ssafy.wine.handler.SingleResult;
 import com.ssafy.wine.repo.UserRepository;
@@ -27,6 +34,9 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping(value = "/user")
 public class SignController {
+
+	@Autowired
+	private JavaMailSender mailSender;
 
 	private final UserRepository userRepository;
 	private final JwtService jwtService;
@@ -48,9 +58,49 @@ public class SignController {
 	public CommonResult signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String email,
 			@ApiParam(value = "비밀번호", required = true) @RequestParam String password,
 			@ApiParam(value = "닉네임", required = true) @RequestParam String nickName) {
+		User user = userRepository.findByEmail(email).orElse(null);
+		if (user != null) {
+			System.out.println(user.getEmail());
+			return responseService.getFailResult(-9999, "중복 회원");
+		}
+
+		// 난수 생성
+		String authkey = new TempKey().getKey(50, false);
 
 		userRepository.save(User.builder().email(email).password(passwordEncoder.encode(password)).nickName(nickName)
-				.roles(Collections.singletonList("USER")).build());
+				.emailCertify(authkey).roles(Collections.singletonList("USER")).build());
+		MailUtils sendMail;
+		try {
+			sendMail = new MailUtils(mailSender);
+			sendMail.setTo(email);
+			sendMail.setSubject("Dionysos 회원가입 이메일 인증");
+			sendMail.setText(new StringBuffer().append("<h1>[이메일 인증]</h1>").append("<p>아래 링크를 클릭하시면 이메일 인증이 완료됩니다.</p>")
+					.append("<a href='http://localhost:8090/WineProject/user/Confirm?email=").append(email)
+					.append("&authkey=").append(authkey).append("' target='_blenk'>이메일 인증 확인</a>").toString());
+			sendMail.setFrom("abc@naver.com", "관리자명");
+			sendMail.send();
+		} catch (MessagingException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return responseService.getSuccessResult();
+	}
+
+	@ApiOperation(value = "이메일 인증", notes = "이메일 인증용")
+	@GetMapping(value = "/Confirm")
+	public CommonResult emailConfirm(@RequestParam String email,
+			@ApiParam(value = "비밀번호", required = true) @RequestParam String authkey) throws Exception {
+		User user = userRepository.findByEmail(email).orElse(null);
+		if (user != null) {
+			if (user.getEmailCertify() == "Y") {
+				return responseService.getSuccessResult();
+			}
+			if (user.getEmailCertify().equals(authkey)) {
+				userRepository.updateEmail(email);
+				return responseService.getSuccessResult();
+			} else
+				return responseService.getFailResult(-9999, "인증키가 다릅니다.");
+		}
+		return responseService.getFailResult(-9999, "회원 검색 실패");
 	}
 }
--- Back_End/src/main/java/com/ssafy/wine/repo/UserRepository.java
+++ Back_End/src/main/java/com/ssafy/wine/repo/UserRepository.java
@@ -3,10 +3,20 @@ package com.ssafy.wine.repo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.wine.dto.User;
 
 // Database에 접근
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
+
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE user u set u.emailcertify = 'Y' where u.email = :email", nativeQuery = true)
+	Integer updateEmail(@Param("email") String email);
+
 }
