ebe36fd7b4beeb1f508882d16c7288ac49f1b2c9
joohongkim1 <wnghd9938@naver.com>
2020-02-06 17:40:50 +0900

    List Page Detail

--- Front_End/src/components/App/Section.tsx
+++ Front_End/src/components/App/Section.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Toolbar from "@material-ui/core/Toolbar";
 import Link from "@material-ui/core/Link";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import brown from '@material-ui/core/colors/brown';
+import brown from "@material-ui/core/colors/brown";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,8 +17,8 @@ const useStyles = makeStyles((theme: Theme) =>
       overflowX: "auto"
     },
     toolbarLink: {
-      fontSize :32,
-      color : brown['900'],
+      fontSize: 32,
+      color: brown["900"],
       padding: theme.spacing(2),
       flexShrink: 0
     }
@@ -26,28 +26,32 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const sections = [
-    { title: '랭킹', url: 'ranking' },
-    { title: '와인 리스트', url: '#' },
-    { title: '와인 리뷰', url: '#' },
-    { title: 'My SNS', url: 'myreview' },
-  ];
+  { title: "랭킹", url: "ranking" },
+  { title: "와인 리스트", url: "list" },
+  { title: "와인 리뷰", url: "#" },
+  { title: "My SNS", url: "myreview" }
+];
 
-  export default function Section() {
+export default function Section() {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-      <div className="container">
-    <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-    {sections.map(section => (
-      <Link
-        color="inherit"
-        noWrap
-        key={section.title}
-        variant="body2"
-        href={section.url}
-        className={classes.toolbarLink}
+  return (
+    <div className="container">
+      <Toolbar
+        component="nav"
+        variant="dense"
+        className={classes.toolbarSecondary}
       >
+        {sections.map(section => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            className={classes.toolbarLink}
+            style={{ textDecoration: "none" }}
+          >
             {section.title}
           </Link>
         ))}
