dc861fed4be90b5375a2f6d4caf4e6cfe67f9780
skydevilk <skydevilk@gmail.com>
2020-02-20 16:38:00 +0900

    post frontEnd

--- Front_End/src/components/MR/Friends/MyFriends.tsx
+++ Front_End/src/components/MR/Friends/MyFriends.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Followers from './Followers';
 import Followings from './Followings';
+import Container from "@material-ui/core/Container";
 
 // Redux
 import { useSelector, useDispatch } from 'react-redux';
 import { rootState } from '../../../../stores/login/store';
-import {UnfollowUserByUID, getUserFollowList, setUserFollowPending } from '../../../../stores/my_sns/actions/follow';
+import { UnfollowUserByUID, getUserFollowList, setUserFollowPending } from '../../../../stores/my_sns/actions/follow';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import Divider from "@material-ui/core/Divider";
 
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    mrFriendsContainer:{
+    mrFriendsContainer: {
       display: "flex",
       paddingTop: "5px",
       // paddingLeft: "24px",
       paddingRight: "24px",
       paddingBottom: "96px",
       marginLeft: "100px",
-      
+
     },
     mrFriends: {
       width: "365px",
@@ -34,7 +36,7 @@ const useStyles = makeStyles((theme: Theme) =>
       fontSize: "12px",
     },
     mrFriendsHeader: {
-      
+
       marginBottom: "0px",
       textAlign: "center",
 
@@ -60,6 +62,9 @@ const useStyles = makeStyles((theme: Theme) =>
     follow: {
       display: "inline-block",
       float: "right",
+    }, divider: {
+      backgroundColor: "#36342f",
+      marginBottom: "80px"
     },
   })
 );
@@ -70,38 +75,42 @@ export default function MyFriends() {
 
   const dispatch = useDispatch();
   //  const [state, setState] = useState(nickname : state.RegistUser.nickname, );
-  const {follow, follower,isUserFollowError, isUserFollowSucceess, isUserFoolowPending } = useSelector(
+  const { follow, follower, isUserFollowError, isUserFollowSucceess, isUserFoolowPending } = useSelector(
     (state: rootState) => state.FollowReducer
   );
-​
-const [followState, setFollowState] = React.useState(false);
 
-const loadFollowList = async () => {
-  console.log("onReviewList");
-  await dispatch(getUserFollowList());
-};
+  const [followState, setFollowState] = React.useState(false);
 
-const unfollow = async (uid : number) => {
-  await dispatch(UnfollowUserByUID(uid));
-  loadFollowList();
-};
+  const loadFollowList = async () => {
+    console.log("onReviewList");
+    await dispatch(getUserFollowList());
+  };
 
-if (!followState) {
-  loadFollowList();
-  setFollowState(true);
-} else {
- 
-}
+  const unfollow = async (uid: number) => {
+    await dispatch(UnfollowUserByUID(uid));
+    loadFollowList();
+  };
+
+  if (!followState) {
+    loadFollowList();
+    setFollowState(true);
+  } else {
+
+  }
 
   return (
-    <div className={classes.mrFriendsContainer}>
-      <div className={classes.mrFriends}>
-      {(function() {
-        
-                if (follower.length > 0) {
-                  return (
-                    <ul className={classes.ulCommon}>
-      
+    <div>
+      <Container>
+        <div className={classes.mrFriendsContainer}>
+          <div style={{ fontWeight: "bold", fontSize: "36px" }}>My Friends</div>
+          <Divider variant="middle" className={classes.divider} />
+          <div className={classes.mrFriends}>
+            {(function () {
+
+              if (follower.length > 0) {
+                return (
+                  <ul className={classes.ulCommon}>
+
                     <h1 className={classes.mrFriendsHeader}>Followers</h1>
                     {follower.map(follower => (<Followers
                       key={follower.uid}
@@ -109,62 +118,59 @@ if (!followState) {
                       email={follower.email}
                       nickName={follower.nickName}
                     />))}
-                    </ul>
-                  );
-                } else {
-                  return(
-                    <ul>
-                      <h1 className={classes.mrFriendsHeader}>Followers</h1>
+                  </ul>
+                );
+              } else {
+                return (
+                  <ul>
+                    <h1 className={classes.mrFriendsHeader}>Followers</h1>
                     <div>팔로우한 유저가 없습니다.</div>
-                    </ul>
-                  );
-                }
-              })()}
+                  </ul>
+                );
+              }
+            })()}
+          </div>
+          <div className={classes.mrFriends}>
+            {(function () {
 
-        
+              if (follow.length > 0) {
+                return (
+                  <ul className={classes.ulCommon}>
+                    <h1 className={classes.mrFriendsHeader}>Followings</h1>
+                    {follow.map(following => (
+                      <div key={following.uid}>
+                        <li key={following.uid} className={classes.liContainer}>
+                          <Link
+                            to={`/friend/${following.uid}`}
+                            style={{ textDecoration: "none" }}
+                          >  {following.nickName}</Link>
 
-    
-      </div>
-      <div className={classes.mrFriends}>
-      {(function() {
-        
-        if (follow.length > 0) {
-          return (
-            <ul className={classes.ulCommon}>
-            <h1 className={classes.mrFriendsHeader}>Followings</h1>
-            {follow.map(following => (
-               <div key={following.uid}>
-               <li key={following.uid} className={classes.liContainer}>
-               <Link
-                    to={`/friend/${following.uid}`}
-                    style={{ textDecoration: "none" }}
-                  >  {following.nickName}</Link>
-            
-            
-                             <Button
-                             variant="contained"
-                             color="primary"
-                             className={classes.follow} onClick={()=> unfollow(following.uid)}
-                           >
-                             팔로우취소
+
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.follow} onClick={() => unfollow(following.uid)}
+                          >
+                            팔로우취소
                            </Button>
-                
-               </li>
-             </div>
-            ))}
-            </ul>
-          );
-        } else {
-          return(
-            <ul>
-              <h1 className={classes.mrFriendsHeader}>Followers</h1>
-            <div>팔로잉한 유저가 없습니다.</div>
-            </ul>
-          );
-        }
-      })()}
-      
-      </div>
+
+                        </li>
+                      </div>
+                    ))}
+                  </ul>
+                );
+              } else {
+                return (
+                  <ul>
+                    <h1 className={classes.mrFriendsHeader}>Followers</h1>
+                    <div>팔로잉한 유저가 없습니다.</div>
+                  </ul>
+                );
+              }
+            })()}
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
