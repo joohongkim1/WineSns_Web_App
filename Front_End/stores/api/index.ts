2caeec1cd5569a2ccfee626e766f08eec0035548
yeseul <seulshine@gmail.com>
2020-03-01 18:35:06 +0900

    서버 주소 변경

--- Front_End/stores/api/index.ts
+++ Front_End/stores/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://i02a303.p.ssafy.io/WineProject',
+    baseURL: 'https://i02a303.p.ssafy.io:8090/WineProject',
     // header: {
     // 	Authorization: 'bearer accessKey'
     // }
