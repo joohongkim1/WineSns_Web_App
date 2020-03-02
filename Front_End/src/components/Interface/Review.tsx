b6eda3c0f752bc25a07205cc2baabdf908e5670c
joohong <wnghd9938@naver.com>
2020-03-03 01:53:54 +0900

    top3 font fix

--- Front_End/src/components/Interface/Review.tsx
+++ Front_End/src/components/Interface/Review.tsx
@@ -1,125 +1,129 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-
-import { Rating } from '@material-ui/lab';
-import Box from '@material-ui/core/Box';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Slide from '@material-ui/core/Slide';
+import { Rating } from "@material-ui/lab";
+import Box from "@material-ui/core/Box";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Slide from "@material-ui/core/Slide";
 import { Link } from "react-router-dom";
 
-import LongMenu from './LongMenu'
+import LongMenu from "./LongMenu";
 
-import '../../index.css';
+import "../../index.css";
 
 export interface Wine {
   wid: number;
-  nameKor : string;
+  nameKor: string;
 }
 
 interface User {
-  uid: number,
-  kakaotalkId: string,
-  naverId: string,
-  googleId: string,
-  facebookId: string,
-  email: string,
-  nickName: string,
+  uid: number;
+  kakaotalkId: string;
+  naverId: string;
+  googleId: string;
+  facebookId: string;
+  email: string;
+  nickName: string;
 }
 
 interface ReviewInfo {
-  user: User,
+  user: User;
   fid: number;
   title: string;
   nameEng: string;
   content: string;
   rating: number;
-  wine: Wine
+  wine: Wine;
 }
-
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
+      height: "100%",
+      display: "flex",
+      flexDirection: "column"
     },
     cardMedia: {
       // width: '100%',
       // maxHeight: 'auto',
-      width: '60px',
+      width: "60px",
       //paddingTop: '56.25%', // 16:9
-      position: 'relative',
-      marginLeft: '40%',
-      height: '255px',
+      position: "relative",
+      marginLeft: "40%",
+      height: "255px"
     },
     cardContent: {
-      flexGrow: 1,
+      flexGrow: 1
     },
     typography: {
-      fontFamily: 'Cafe24Shiningstar',
+      fontFamily: "Cafe24Shiningstar"
     },
     more: {
       "& > *": {
         margin: theme.spacing(1, 8)
       },
-      fontSize: '18px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
+      fontSize: "18px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
     },
     center: {
-      fontSize: '18px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
+      fontSize: "18px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
     }
-  }),
+  })
 );
 
 function ReviewInfo(review: ReviewInfo) {
   const classes = useStyles();
 
   const [checked] = React.useState(true);
-  const userData = sessionStorage.getItem('uid'); // string
-  const feedUserData = String(review.user.uid) //as unknown as string // number
+  const userData = sessionStorage.getItem("uid"); // string
+  const feedUserData = String(review.user.uid); //as unknown as string // number
 
   return (
-    <Slide direction="up" in={checked} mountOnEnter unmountOnExit >
+    <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
       {/* <Grid item xs={12} sm={6} md={4}> */}
       <Grid item xs={4}>
         <Card className={classes.card}>
           <CardMedia
-
             className={classes.cardMedia}
             image={`http://i02a303.p.ssafy.io:8090/WineProject/Wine/${review.nameEng}.gif`}
             title="Image title"
           />
           <CardContent className={classes.cardContent}>
+            <Typography className={classes.center}>{review.title}</Typography>
             <Typography className={classes.center}>
-              {review.title}
-            </Typography>
-            <Typography className={classes.center}>
-              <div dangerouslySetInnerHTML={{ __html: review.content.substring(0, 80) }}>
-
-              </div>
-
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: review.content.substring(0, 80)
+                }}
+              ></div>
             </Typography>
           </CardContent>
 
           <Box component="fieldset" mb={3} borderColor="transparent">
-            <Rating name="read-only" value={review.rating} readOnly className={classes.center} />
+            <Rating
+              name="read-only"
+              value={review.rating}
+              readOnly
+              className={classes.center}
+            />
           </Box>
           <div className={classes.more}>
-            <Link to={`/review/${review.fid}`} style={{ textDecoration: "none" }}>
+            <Link
+              to={`/review/${review.fid}`}
+              style={{ textDecoration: "none" }}
+            >
               <Button variant="outlined">View More</Button>
             </Link>
             {/* {userData === feedUserData &&
@@ -127,9 +131,7 @@ function ReviewInfo(review: ReviewInfo) {
                 <LongMenu review={review}/>
               </div>
             } */}
-            
           </div>
-
         </Card>
       </Grid>
     </Slide>
--- Front_End/src/components/Interface/Wine.tsx
+++ Front_End/src/components/Interface/Wine.tsx
@@ -1,100 +1,91 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-
-import { Rating } from '@material-ui/lab';
-import Box from '@material-ui/core/Box';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Slide from '@material-ui/core/Slide';
-import {Link} from "react-router-dom";
-import '../../index.css';
-
+import { Rating } from "@material-ui/lab";
+import Box from "@material-ui/core/Box";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Slide from "@material-ui/core/Slide";
+import { Link } from "react-router-dom";
+import "../../index.css";
 
 interface WineInfo {
-    wid : number; 
-    nameKor: string;
-    nameEng : string;
-    type : string;
-    info: string;
+  wid: number;
+  nameKor: string;
+  nameEng: string;
+  type: string;
+  info: string;
 }
-
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    // width: '100%',
-    // maxHeight: 'auto',
-    width:'70px',
-    //paddingTop: '56.25%', // 16:9
-    position: 'relative',
-    marginLeft : '40%',
-    height: '230px',
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  typography: {
-    fontFamily: 'Cafe24Shiningstar',
-  },                                                                                                                                 
-}),
+    card: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column"
+    },
+    cardMedia: {
+      // width: '100%',
+      // maxHeight: 'auto',
+      width: "70px",
+      //paddingTop: '56.25%', // 16:9
+      position: "relative",
+      marginLeft: "40%",
+      height: "230px"
+    },
+    cardContent: {
+      flexGrow: 1
+    },
+    typography: {
+      fontFamily: "S-CoreDream-8Heavy"
+    }
+  })
 );
 
 function WineInfo(wine: WineInfo) {
   const classes = useStyles();
 
   const [checked] = React.useState(true);
-  
 
-    return (
-      <Slide direction="up" in={checked} mountOnEnter unmountOnExit >
-       
-        <Grid item xs={12} sm={6} md={4}>
+  return (
+    <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
+      <Grid item xs={12} sm={6} md={4}>
         <Link to={`/detail/${wine.wid}`} style={{ textDecoration: "none" }}>
-            <Card className={classes.card}>
-                <CardMedia
-                    
-                    className={classes.cardMedia}
-                    image={`http://i02a303.p.ssafy.io:8090/WineProject/Wine/${wine.nameEng}.gif`}
-                    title="Image title"
-                />
-                <CardContent className={classes.cardContent}>
-                    <Typography className={classes.typography} >
-                     {wine.nameKor}
-      </Typography>
-      <Typography className={classes.typography}>
-                     {wine.nameEng}
-      </Typography>
-                    <Typography>
-                        {wine.type}
-      </Typography>
-      <Typography>
-                        {wine.info.substring(0,80)}...
-      </Typography>
-                </CardContent>
-               
-                    {/* <Button size="small" color="primary">
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.cardMedia}
+              image={`http://i02a303.p.ssafy.io:8090/WineProject/Wine/${wine.nameEng}.gif`}
+              title="Image title"
+            />
+            <CardContent className={classes.cardContent}>
+              <Typography className={classes.typography}>
+                {wine.nameKor.substring(0, 20)}
+              </Typography>
+              <Typography className={classes.typography}>
+                {wine.nameEng.substring(0, 25)}
+              </Typography>
+              <br />
+              <Typography>
+                <em className={`tag type ${wine.type}`}> {wine.type}</em>
+              </Typography>
+              <br />
+              <Typography>{wine.info.substring(0, 80)}...</Typography>
+            </CardContent>
+
+            {/* <Button size="small" color="primary">
                         View
       </Button> */}
-    
-              
-            </Card>
-            </Link>
-        </Grid>
-  
-        </Slide>
-    );
+          </Card>
+        </Link>
+      </Grid>
+    </Slide>
+  );
 }
 
 export default WineInfo;
